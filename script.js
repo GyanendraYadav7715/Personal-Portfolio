431f7ecad954f15a69df7f4f32189ecc2c175cae
Gyan Yadav <codeaura108@gmail.com>
2024-01-10 23:59:58 +0530

    done

---
+++ script.js
@@ -0,0 +1,7 @@
+// var typed =new Typed(".text",{
+//     strings:["Frontend Developer","Youtuber","Backend Developer" ],
+//      speed:100,
+//      typespeed:100,
+     
+//     loop:true
+// });
